6e96e0e1d621e7c8c36005680e1c9022d8dce600
Kiran <kirankumardn74@gmail.com>
2024-06-25 23:12:32 +0530

    files updated

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('hello world');
 });
+app.get('/user/:id', async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    console.log('error', error);
+  }
+  res.send('hello world');
+});
 
 app.listen(port, () => {
   console.log('app is up and running in the port 3000');
